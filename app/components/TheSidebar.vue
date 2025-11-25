f663939148671923ce6d1e3ba5b033f6fabc9338
Igor Fronza <igorfronza20@gmail.com>
2025-11-25 09:49:07 -0300

    removido mercado pago e ajustado login

--- app/components/TheSidebar.vue
+++ app/components/TheSidebar.vue
@@ -36,7 +36,7 @@
                             <NuxtLink @click="$emit('closeSidebar')" to="/services" class="flex items-center p-3 text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 rounded-lg transition duration-200">Serviços</NuxtLink>
                             <NuxtLink @click="$emit('closeSidebar')" to="/booking" v-if="session?.user.id" class="flex items-center p-3 text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 rounded-lg transition duration-200">Agendamentos</NuxtLink>
                         </nav>
-                        <nav class="space-y-3" v-if="session.user.isAdmin">
+                        <nav class="space-y-3" v-if="session?.user.isAdmin">
                             <hr class="my-2" />
                             <NuxtLink @click="$emit('closeSidebar')" to="/allBooking" class="flex items-center p-3 text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 rounded-lg transition duration-200">Todos os Agendamentos</NuxtLink>
                         </nav>
